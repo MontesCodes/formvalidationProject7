fe6bd8d6f2b60292410f16bfb93fac3140d7224d
MontiesCodes_LT-1557 <MontiesWebDev@Gmail.com>
2024-05-20 06:57:29 -0400

    updated project7 JS time 15:16

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const form = document.getElementById('form');
+const userName = document.querySelector('.userName');
+const userEmail = document.querySelector('.userEmail');
+const userPassword = document.querySelector('.userPassword');
+const userConPassword = document.querySelector('.userConPassword');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  validInput();
 });
+
+const validInput = () => {
+  const userElement = userName.value.trim();
+  console.log(userElement);
+};
