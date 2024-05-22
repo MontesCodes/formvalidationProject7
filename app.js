bdce51959e80db707a691aa7d2a8088e94d6f50b
MontiesCodes_LT-1557 <MontiesWebDev@Gmail.com>
2024-05-22 07:32:29 -0400

    updated project7 JS time 17:36

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const form = document.getElementById('form');
 const userName = document.querySelector('.userName');
 const userEmail = document.querySelector('.userEmail');
 const userPassword = document.querySelector('.userPassword');
-const userConPassword = document.querySelector('.userConPassword');
+const userConfPassword = document.querySelector('.userConPassword');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   validInput();
 });
 
+const showError = (Element, message) => {
+  const inputControl = Element.parentElement;
+};
+
 const validInput = () => {
   const userElement = userName.value.trim();
-  console.log(userElement);
+  const emailElement = userEmail.value.trim();
+  const passwordElement = userPassword.value.trim();
+  const confPasswordElement = userConfPassword.value.trim();
+  // console.log(emailElement);
+
+  if (userElement === '') {
+  }
 };
